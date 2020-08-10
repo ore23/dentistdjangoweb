e86b79eb243e69113a1e149473f521688b81c5f0
ore23 <ore11su11@gmail.com>
2020-08-10 11:45:45 +0300

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+
+def home(request):
+    return render(request, 'home.html', {})
+
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        send_mail(
+            message_name,
+            message,
+            message_email,
+            ['soultatisorestis@gmail.com']
+        )
+
+
+        return render(request, 'contact.html', {'message_name' : message_name})
+    else:
+        return render(request, 'contact.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def pricing(request):
+    return render(request, 'pricing.html', {})
+
+
+def service(request):
+    return render(request, 'service.html', {})
+
+def appointment(request):
+
+    if request.method == "POST":
+
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_address = request.POST['your-address']
+        your_schedule = request.POST['your-schedule']
+        your_date = request.POST['your-date']
+        your_message = request.POST['your-message']
+
+        appointment = 'Name: ' + your_name + 'Phone :' + your_phone + 'email :' + your_email
+
+        send_mail(
+            'Appointment Request',
+            appointment,
+            your_email,
+            ['soultatisorestis@gmail.com']
+        )
+
+
+        return render(request, 'appointment.html', {
+                        'your_name' :your_name,
+                        'your_phone': your_phone,
+                        'your_email': your_email,
+                        'your_address':your_address,
+                        'your_schedule':your_schedule,
+                        'your_date':your_date,
+        })
+    else:
+        return render(request, 'home.html', {})
